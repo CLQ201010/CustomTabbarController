a0b5c759f49f4a48698f906253c88e0aa428c3fe
CLQ201010 <499897572@qq.com>
2016-08-29 13:52:18 +0800

    Initial Commit

---
+++ CustomTabbarController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomTabbarController
+//
+//  Created by ccq chen on 16/8/29.
+//  Copyright © 2016年 ccq chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
